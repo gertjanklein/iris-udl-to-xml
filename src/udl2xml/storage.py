2987d07628044b8abb96c7104608f7572fdca95f
Gertjan Klein <gklein@xs4all.nl>
2024-05-27 22:12:43 +0200

    Add sorting of storage XML
    The storage XML, as displayed in the UDL class definition, is ordered
    differently than the storage as exported in XML. The former appears to be
    ordered alphabetically, the latter according to definitions in %occKeyword.
    This commit handles the top-level elements and Property subelements.

--- src/udl2xml/storage.py
+++ src/udl2xml/storage.py
@@ -35,6 +35,54 @@ def handle_storage(cls:etree._Element, stream:StringIO, line:str, doc:str|None):
     # To get at them, wrap these XML elements inside a temporary root.
     storage = f"<root___temp>{'\n'.join(lines)}\n</root___temp>"
     root = etree.fromstring(storage)
+    
+    # Sort for compatibility with IRIS export. In UDL, the elements are ordered
+    # alphabetically, but in an XML export they are ordered according to ^oddCOM.
+    root[:] = sorted(root, key=lambda el: ORDER.get(el.tag, 999))
+    for el in root:
+        # Property subelements are also sorted differently
+        if el.tag == 'Property':
+            el[:] = sorted(el, key=lambda el: ORDER_PROP.get(el.tag, 999))
+    
     # Add subelements of the temporary root to our Storage element
     for el in root:
         stg.append(el)
+
+
+ORDER = {
+    "Description": 4,
+    "Type": 5,
+    "Final": 7,
+    "Internal": 14,
+    "Deprecated": 17,
+    "DataLocation": 21,
+    "DefaultData": 22,
+    "IdExpression": 23,
+    "IdLocation": 24,
+    "IndexLocation": 25,
+    "SqlChildSub": 27,
+    "SqlIdExpression": 28,
+    "SqlRowIdName": 29,
+    "SqlRowIdProperty": 30,
+    "SqlTableNumber": 31,
+    "State": 32,
+    "StreamLocation": 33,
+    "VersionLocation": 35,
+    "CounterLocation": 36,
+    "IdFunction": 37,
+    "ExtentLocation": 38,
+    "ExtentSize": 34,
+    "Sharded": 40,
+}
+
+ORDER_PROP = {
+    "Selectivity": 21,
+    "StreamLocation": 22,
+    "ChildExtentSize": 23,
+    "OutlierSelectivity": 24,
+    "BiasQueriesAsOutlier": 25,
+    "AverageFieldSize": 26,
+    "ChildBlockCount": 27,
+    "Histogram": 28,
+}
+
